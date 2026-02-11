f3091f0c291940c9049c0351308f32c4711a7e18
Monirul Islam <mmislam027@gmail.com>
2026-02-11 22:10:13 +0600

    fix: remove bkash module because of long payment process

--- src/modules/payment/providers/providers.module.ts
+++ src/modules/payment/providers/providers.module.ts
@@ -1,34 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { BkashModule } from './bkash/bkash.module';
 import { SslcomerzModule } from './sslcomerz/sslcomerz.module';
 import { StrapiModule } from './strapi/strapi.module';
 
 @Module({
   imports: [
-    BkashModule,
     SslcomerzModule.registerAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        // console.log(configService);
-        const d = {
-          store_id: configService.getOrThrow<string>('sslcomerz.store_id'),
-          store_password: configService.getOrThrow<string>(
-            'sslcomerz.store_password',
-          ),
-          store_type: configService.getOrThrow<string>('sslcomerz.store_type'),
-          is_live: configService.getOrThrow<boolean>('sslcomerz.is_live'),
-          success_url: configService.getOrThrow<string>(
-            'sslcomerz.success_url',
-          ),
-          failure_url: configService.getOrThrow<string>(
-            'sslcomerz.failure_url',
-          ),
-          cancel_url: configService.getOrThrow<string>('sslcomerz.cancel_url'),
-          ipn_url: configService.getOrThrow<string>('sslcomerz.ipn_url'),
-        };
-        return d;
-      },
+      useFactory: (configService: ConfigService) => ({
+        store_id: configService.getOrThrow<string>('sslcomerz.store_id'),
+        store_password: configService.getOrThrow<string>(
+          'sslcomerz.store_password',
+        ),
+        store_type: configService.getOrThrow<string>('sslcomerz.store_type'),
+        is_live: configService.getOrThrow<boolean>('sslcomerz.is_live'),
+        success_url: configService.getOrThrow<string>('sslcomerz.success_url'),
+        failure_url: configService.getOrThrow<string>('sslcomerz.failure_url'),
+        cancel_url: configService.getOrThrow<string>('sslcomerz.cancel_url'),
+        ipn_url: configService.getOrThrow<string>('sslcomerz.ipn_url'),
+      }),
     }),
     StrapiModule,
   ],
