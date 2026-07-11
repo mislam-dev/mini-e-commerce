50f4641cd375911112be22337065b3cae2b7824e
Monirul Islam <mmislam027@gmail.com>
2026-07-11 20:59:57 +0600

    feat: add caching support for product and category use cache aside caching strategy for caching data

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,25 +1,41 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { type Cache } from 'cache-manager';
 import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { Category } from './entities/category.entity';
+
+const CACHE_KEY = 'CATEGORY_TREE';
+const CACHE_TTL = 60 * 60 * 1000; // 1 hour
 
 @Injectable()
 export class CategoryService {
   constructor(
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
     const category = this.categoryRepository.create(createCategoryDto);
+    await this.cacheManager.del(CACHE_KEY);
     return await this.categoryRepository.save(category);
   }
 
   async findAll() {
+    const cachedCategories = await this.cacheManager.get<any[]>(CACHE_KEY);
+
+    if (cachedCategories) {
+      return cachedCategories;
+    }
+
     const categories = await this.categoryRepository.find();
-    return this.buildTree(categories);
+    const tree = this.buildTree(categories);
+    await this.cacheManager.set(CACHE_KEY, tree, CACHE_TTL);
+
+    return tree;
   }
 
   private buildTree(
@@ -48,11 +64,13 @@ export class CategoryService {
   ): Promise<Category> {
     const category = await this.findOne(id);
     Object.assign(category, updateCategoryDto);
+    await this.cacheManager.del(CACHE_KEY);
     return await this.categoryRepository.save(category);
   }
 
   async remove(id: string): Promise<void> {
     const result = await this.categoryRepository.delete(id);
+    await this.cacheManager.del(CACHE_KEY);
     if (result.affected === 0) {
       throw new NotFoundException(`Category with ID ${id} not found`);
     }
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { type Cache } from 'cache-manager';
 import { PaginationDto } from 'src/common/pagination/pagination.dto';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -7,15 +9,20 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { UpdateStockDto } from './dto/update-stock.dto';
 import { Product } from './entities/product.entity';
 
+const PRODUCT_KEY = 'products';
+const PRODUCT_TTL = 60 * 60 * 1000; // 1 hour
+
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
   ) {}
 
   async create(createProductDto: CreateProductDto): Promise<Product> {
     const product = this.productRepository.create(createProductDto);
+    await this.cacheManager.del(PRODUCT_KEY);
     return this.productRepository.save(product);
   }
 
@@ -25,19 +32,34 @@ export class ProductService {
     limit: number;
     offset: number;
   }> {
+    const cache: any = await this.cacheManager.get(PRODUCT_KEY);
+    if (cache) {
+      return cache;
+    }
     const { limit = 10, offset = 0 } = pagination;
     const [results, total] = await this.productRepository.findAndCount({
       take: limit,
       skip: offset,
     });
-    return { results, total, limit, offset };
+    const data = { results, total, limit, offset };
+    await this.cacheManager.set(PRODUCT_KEY, data, PRODUCT_TTL);
+    return data;
   }
 
   async findOne(id: string): Promise<Product> {
+    const cache = await this.cacheManager.get(this.singleProductKey(id));
+    if (cache) {
+      return cache as Product;
+    }
     const product = await this.productRepository.findOneBy({ id });
     if (!product) {
       throw new NotFoundException(`Product with ID ${id} not found`);
     }
+    await this.cacheManager.set(
+      this.singleProductKey(id),
+      product,
+      PRODUCT_TTL,
+    );
     return product;
   }
 
@@ -47,7 +69,12 @@ export class ProductService {
   ): Promise<Product> {
     const product = await this.findOne(id);
     Object.assign(product, updateProductDto);
-    return this.productRepository.save(product);
+    const d = await this.productRepository.save(product);
+
+    await this.cacheManager.del(this.singleProductKey(id));
+    await this.cacheManager.del(PRODUCT_KEY);
+
+    return d;
   }
 
   async remove(id: string): Promise<void> {
@@ -55,6 +82,8 @@ export class ProductService {
     if (result.affected === 0) {
       throw new NotFoundException(`Product with ID ${id} not found`);
     }
+    await this.cacheManager.del(this.singleProductKey(id));
+    await this.cacheManager.del(PRODUCT_KEY);
   }
 
   async updateStock(
@@ -63,6 +92,14 @@ export class ProductService {
   ): Promise<Product> {
     const product = await this.findOne(id);
     product.stockQuantity = updateStockDto.quantity;
-    return this.productRepository.save(product);
+    const data = this.productRepository.save(product);
+
+    await this.cacheManager.del(this.singleProductKey(id));
+    await this.cacheManager.del(PRODUCT_KEY);
+
+    return data;
+  }
+  private singleProductKey(id: string) {
+    return `products:${id}`;
   }
 }
